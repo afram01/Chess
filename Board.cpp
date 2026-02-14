752c55968a2d349604415d9cc8a76739761c63af
40312358002 <Rania.Afsharian@gmail.com>
2026-02-15 01:27:35 +0330

    Expansion board-3 and type

--- Board.cpp
+++ Board.cpp
@@ -96,3 +96,129 @@ Board& Board::operator=(const Board& other) {
     return *this;
 }
 
+
+void Board::initialize() {
+    clear();
+    
+    for (int c = 0; c < 8; c++) {
+        grid[1][c] = std::make_unique<Pawn>(Color::WHITE, Position(1, c));
+    }
+    
+    for (int c = 0; c < 8; c++) {
+        grid[6][c] = std::make_unique<Pawn>(Color::BLACK, Position(6, c));
+    }
+    
+    grid[0][0] = std::make_unique<Rook>(Color::WHITE, Position(0, 0));
+    grid[0][7] = std::make_unique<Rook>(Color::WHITE, Position(0, 7));
+    grid[7][0] = std::make_unique<Rook>(Color::BLACK, Position(7, 0));
+    grid[7][7] = std::make_unique<Rook>(Color::BLACK, Position(7, 7));
+    
+    grid[0][1] = std::make_unique<Knight>(Color::WHITE, Position(0, 1));
+    grid[0][6] = std::make_unique<Knight>(Color::WHITE, Position(0, 6));
+    grid[7][1] = std::make_unique<Knight>(Color::BLACK, Position(7, 1));
+    grid[7][6] = std::make_unique<Knight>(Color::BLACK, Position(7, 6));
+    
+    grid[0][2] = std::make_unique<Bishop>(Color::WHITE, Position(0, 2));
+    grid[0][5] = std::make_unique<Bishop>(Color::WHITE, Position(0, 5));
+    grid[7][2] = std::make_unique<Bishop>(Color::BLACK, Position(7, 2));
+    grid[7][5] = std::make_unique<Bishop>(Color::BLACK, Position(7, 5));
+    
+    grid[0][3] = std::make_unique<Queen>(Color::WHITE, Position(0, 3));
+    grid[7][3] = std::make_unique<Queen>(Color::BLACK, Position(7, 3));
+    
+    grid[0][4] = std::make_unique<King>(Color::WHITE, Position(0, 4));
+    grid[7][4] = std::make_unique<King>(Color::BLACK, Position(7, 4));
+    
+    whiteKingMoved = false;
+    blackKingMoved = false;
+    whiteKingsideRookMoved = false;
+    whiteQueensideRookMoved = false;
+    blackKingsideRookMoved = false;
+    blackQueensideRookMoved = false;
+}
+
+void Board::initializeWithSpecialPieces() {
+    clear();
+    
+    grid[1][0] = std::make_unique<SoldierPlus>(Color::WHITE, Position(1, 0));
+    grid[1][1] = std::make_unique<Pawn>(Color::WHITE, Position(1, 1));
+    grid[1][2] = std::make_unique<Pawn>(Color::WHITE, Position(1, 2));
+    grid[1][3] = std::make_unique<Joker>(Color::WHITE, Position(1, 3));
+    grid[1][4] = std::make_unique<Pawn>(Color::WHITE, Position(1, 4));
+    grid[1][5] = std::make_unique<Pawn>(Color::WHITE, Position(1, 5));
+    grid[1][6] = std::make_unique<Pawn>(Color::WHITE, Position(1, 6));
+    grid[1][7] = std::make_unique<Pawn>(Color::WHITE, Position(1, 7));
+    
+    grid[6][0] = std::make_unique<Pawn>(Color::BLACK, Position(6, 0));
+    grid[6][1] = std::make_unique<Pawn>(Color::BLACK, Position(6, 1));
+    grid[6][2] = std::make_unique<Pawn>(Color::BLACK, Position(6, 2));
+    grid[6][3] = std::make_unique<Joker>(Color::BLACK, Position(6, 3));
+    grid[6][4] = std::make_unique<SoldierPlus>(Color::BLACK, Position(6, 4));
+    grid[6][5] = std::make_unique<Pawn>(Color::BLACK, Position(6, 5));
+    grid[6][6] = std::make_unique<Pawn>(Color::BLACK, Position(6, 6));
+    grid[6][7] = std::make_unique<Pawn>(Color::BLACK, Position(6, 7));
+    
+    grid[0][0] = std::make_unique<Rook>(Color::WHITE, Position(0, 0));
+    grid[0][7] = std::make_unique<Rook>(Color::WHITE, Position(0, 7));
+    grid[7][0] = std::make_unique<Rook>(Color::BLACK, Position(7, 0));
+    grid[7][7] = std::make_unique<Rook>(Color::BLACK, Position(7, 7));
+    
+    grid[0][1] = std::make_unique<Knight>(Color::WHITE, Position(0, 1));
+    grid[0][6] = std::make_unique<Knight>(Color::WHITE, Position(0, 6));
+    grid[7][1] = std::make_unique<Knight>(Color::BLACK, Position(7, 1));
+    grid[7][6] = std::make_unique<Knight>(Color::BLACK, Position(7, 6));
+    
+    grid[0][2] = std::make_unique<Bishop>(Color::WHITE, Position(0, 2));
+    grid[0][5] = std::make_unique<Bishop>(Color::WHITE, Position(0, 5));
+    grid[7][2] = std::make_unique<Bishop>(Color::BLACK, Position(7, 2));
+    grid[7][5] = std::make_unique<Bishop>(Color::BLACK, Position(7, 5));
+    
+    grid[0][3] = std::make_unique<ArmoredQueen>(Color::WHITE, Position(0, 3));
+    grid[7][3] = std::make_unique<ArmoredQueen>(Color::BLACK, Position(7, 3));
+    
+    grid[0][4] = std::make_unique<King>(Color::WHITE, Position(0, 4));
+    grid[7][4] = std::make_unique<King>(Color::BLACK, Position(7, 4));
+    
+    grid[2][4] = std::make_unique<Spy>(Color::WHITE, Color::BLACK, Position(2, 4));
+    grid[5][3] = std::make_unique<Spy>(Color::BLACK, Color::WHITE, Position(5, 3));
+    
+    armoredQueenHits[Position(0, 3)] = 0;
+    armoredQueenHits[Position(7, 3)] = 0;
+    jokerTransformsUsed[Position(1, 3)] = 0;
+    jokerTransformsUsed[Position(6, 3)] = 0;
+    
+    whiteKingMoved = false;
+    blackKingMoved = false;
+    whiteKingsideRookMoved = false;
+    whiteQueensideRookMoved = false;
+    blackKingsideRookMoved = false;
+    blackQueensideRookMoved = false;
+}
+
+void Board::clear() {
+    for (int r = 0; r < 8; r++) {
+        for (int c = 0; c < 8; c++) {
+            grid[r][c].reset();
+        }
+    }
+    moveHistory.clear();
+    enPassantAvailable = false;
+    enPassantSquare = Position();
+    pieceStationaryCounter.clear();
+    armoredQueenHits.clear();
+    jokerTransformsUsed.clear();
+    lastCapturedPiece.reset();
+    
+    whiteKingMoved = false;
+    blackKingMoved = false;
+    whiteKingsideRookMoved = false;
+    whiteQueensideRookMoved = false;
+    blackKingsideRookMoved = false;
+    blackQueensideRookMoved = false;
+    
+    whiteQueenCanDoubleMove = false;
+    blackQueenCanDoubleMove = false;
+    
+    currentSeason = Season::NONE;
+    movesSinceSeasonChange = 0;
+}
---
+++ Types.hpp
@@ -0,0 +1,47 @@
+#ifndef TYPES_HPP
+#define TYPES_HPP
+
+#include "Position.hpp"
+#include "Piece.hpp"
+
+enum Season {
+    SPRING,   
+    SUMMER,  
+    AUTUMN, 
+    WINTER  
+};
+
+enum GameStatus {
+    PLAYING,
+    CHECK,
+    CHECKMATE,
+    STALEMATE,
+    DRAW
+};
+
+enum GameModeType {
+    STANDARD,
+    ENERGY,
+    MISSION
+};
+
+struct MoveRecord {
+    Move move;
+    PieceType movedPiece;
+    PieceType capturedPiece;
+    Color playerColor;
+    std::string notation;
+    bool wasCastling;
+    bool wasEnPassant;
+    bool wasPromotion;
+    
+    MoveRecord() 
+        : movedPiece(PieceType::PAWN),
+          capturedPiece(PieceType::PAWN),
+          playerColor(Color::WHITE),
+          wasCastling(false),
+          wasEnPassant(false),
+          wasPromotion(false) {}
+};
+
+#endif 
