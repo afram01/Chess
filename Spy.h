fdb9494f8c838cdb93a8d1540a9fc48354179ef2
afram01 <ramezanyaida84@gmail.com>
2026-02-16 15:45:54 +0400

    Add Spy special piece that disguises as enemy pieces

---
+++ Spy.h
@@ -0,0 +1,40 @@
+#ifndef SPY_H
+#define SPY_H
+
+#include "piece.h"
+#include <memory>
+#include <random>
+
+class Spy : public Piece {
+private:
+    Color realOwner;        
+    bool revealed;          
+    std::unique_ptr<Piece> disguisedPiece; 
+    int moveCounter;         
+    static constexpr int REVEAL_THRESHOLD = 5; 
+
+public:
+    Spy(Color realOwner, std::unique_ptr<Piece> pieceToDisguise);
+    
+    virtual bool isValidMove(int fromRow, int fromCol, int toRow, int toCol, const Board& board) const override;
+    virtual std::vector<Position> getPossibleMoves(int row, int col, const Board& board) const override;
+    virtual bool hasSpecialAbility() const override;
+    virtual void useSpecialAbility(int row, int col, Board& board) override;
+    virtual std::string getSymbol() const override;
+    virtual std::unique_ptr<Piece> clone() const override;
+    virtual std::string serialize() const override;
+    
+    void incrementMoveCounter();
+    
+    void reveal();
+    
+    bool isRevealed() const;
+    Color getRealOwner() const;
+    PieceType getDisguisedAs() const;
+    Color getDisplayColor() const;
+    int getMovesUntilReveal() const;
+    
+    const Piece* getDisguisedPiece() const { return disguisedPiece.get(); }
+};
+
+#endif
